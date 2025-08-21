e4de18bd5fcc6ff84432c3a3e3eae8245b749472
Kumari040 <kms745362@gmail.com>
2025-08-22 05:26:01 +0530

    deletion

--- backend/models/timetable.js
+++ backend/models/timetable.js
@@ -5,7 +5,9 @@ const timetableSchema=new mongoose.Schema({
     classroomId:{type:mongoose.Schema.Types.ObjectId,ref:'classroom',required:true},
     teacherId:{type:mongoose.Schema.Types.ObjectId, ref:'user',required:true},
     time:{type:String,required:true},
+    
     day:{type:String,required:true}
 
 });
+
 module.exports=mongoose.model('timetable',timetableSchema);
